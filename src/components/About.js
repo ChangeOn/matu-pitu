1776515f9d210812e4b71460cc826863e150913a
Changeon.Kim <lygerastia5@gmail.com>
2021-05-26 14:43:41 +0900

    initial commit. - react router

---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function About() {
+    return (
+        <div>
+            <h3><strong>Matu-Pitu</strong> | Grammer Checker Lite.</h3>
+            <p>
+                Supported and Inspired by Jade, Go and Dev.
+            </p>
+        </div>
+    )
+}
+
+export default About;
---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect} from 'react';
+const hanspell = require('hanspell');
+
+function Main() {
+    return (
+        <div>
+            <h1>한국어 맞춤법 검사기 lite</h1>
+            <h2>Matu Pitu(machu picchu)</h2>
+        </div>
+    );
+}
+
+export default Main;
